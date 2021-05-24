9ba0acdfcb195ccb6e47d31e967b114168157cd7
Sasa M <smikodanic@gmail.com>
2021-05-24 14:24:51 +0200

    Echo eventEmitter

--- helpers/echo/Echo.js
+++ helpers/echo/Echo.js
@@ -1,11 +1,8 @@
 /**
- * DEX8 library for sending messages, objekts and errors via websocket or to console.
+ * DEX8 library for logging echo messages.
  * Echo, websocket message format:
- * {
- *  user_id,
- *  robot_id,
- *  task_id,
- *  echo_method: 'log'|'objekt'|'error'|'image'|'input',
+ * echoObj:: {
+ *  echo_method: 'log'|'objekt'|'error'|'image',
  *  echo_msg: any,
  *  time: string
  * }
@@ -13,30 +10,15 @@
 
 const chalk = require('chalk');
 const moment = require('moment');
-const inquirer = require('inquirer');
+const EventEmitter = require('events');
 
 
 
 class Echo {
 
-  constructor(user_id, robot_id, task_id, wsClient) {
-    this.user_id = user_id;
-    this.robot_id = robot_id;
-    this.task_id = task_id;
-    this.wsClient = wsClient;
-
-    // short console log -> string message instead of whole object in __console_log() method
-    this.short = false;
-
-    // initialize message object
-    this.msgObj = {
-      user_id,
-      robot_id,
-      task_id,
-      echo_method: '', // log, error, objekt, error, image, input
-      echo_msg: '',
-      time: ''
-    };
+  constructor() {
+    this.eventEmitter = new EventEmitter; // send echo message to the robot
+    this.short = false; // short log message
   }
 
 
@@ -61,9 +43,9 @@ class Echo {
     });
 
     const str = strings.join(' '); // join with space  ::  echo.log('a', 'b') ---> ['a', 'b'] ---> 'a b'
-    this._format('log', str);
-    this._log();
-    return this.msgObj;
+    const echoObj = this._format('log', str);
+    this._log(echoObj);
+    return echoObj;
   }
 
 
@@ -74,9 +56,9 @@ class Echo {
    * @return {any}
    */
   objekt(obj) {
-    this._format('objekt', obj);
-    this._log();
-    return this.msgObj;
+    const echoObj = this._format('objekt', obj);
+    this._log(echoObj);
+    return echoObj;
   }
 
 
@@ -87,9 +69,9 @@ class Echo {
    * @return {any}
    */
   error(err) {
-    this._format('error', err);
-    this._log();
-    return this.msgObj;
+    const echoObj = this._format('error', err);
+    this._log(echoObj);
+    return echoObj;
   }
 
 
@@ -99,132 +81,70 @@ class Echo {
    * @param {String} img_b64 - image in the base64 (string) format
    * @return {any}
    */
-  image(img_b64) {
-    this._format('image', img_b64);
-    this._log();
-    return this.msgObj;
-  }
-
-
-  /**
-   * Method used in task functions as echo.input();
-   * Send input which should be listened with the listen() method.
-   * @param {String} inp - text in front of the input field (label)
-   * @return {any}
-   */
-  input(inp) {
-    this._format('input', inp);
-    this._log();
-    return this.msgObj;
-  }
-
-
-  /**
-   * Listen for the first message from the web panel or API
-   * @return {Promise<any>} - can be used in async function as "const msg = await echo.listen()"
-   */
-  async listen() {
-    if (!!this.wsClient) { // listen input from the Web Panel
-      return new Promise((resolve, reject) => {
-        this.wsClient.once('message', (msg, msgSTR, msgBUF) => {
-          resolve(msgSTR);
-        });
-        setTimeout(() => {
-          reject(new Error('Echo input stopped to listen due to timeout of 1 minute.'));
-        }, 60000);
-      });
-    } else { // listen input from the Linux Console
-      const questions = [
-        {type: 'input', name: 'inp', message: 'input:', default: ''}
-      ];
-      const answers =  await inquirer.prompt(questions);
-      return answers.inp;
-    }
+  img(img_b64) {
+    const echoObj = this._format('image', img_b64);
+    this._log(echoObj);
+    return echoObj;
   }
 
 
 
 
   /******** PRIVATE METHODS  *******/
-
   /**
    * Format object which is sent by websocket or to linux console.
-   * @param {String} echo_method - used echo method: log, error, objekt, ...
+   * @param {String} echo_method - used echo method: log, error, objekt, image
    * @param {String} echo_msg - the echo message
    */
   _format(echo_method, echo_msg) {
     if (echo_method === 'error') {
-      // convert error to object with two properties: message: string and stack: string
+      // convert error to object with two properties: message:string and stack:string
       echo_msg = {
         message: echo_msg.message,
         stack: echo_msg.stack
       };
     }
-
     const time = moment().toISOString();
-
-    this.msgObj = Object.assign(this.msgObj, {echo_method, echo_msg, time}); // {user_id, robot_id, task_id, echo_method, echo_message, time}
+    const echoObj = {echo_method, echo_msg, time};
+    return echoObj;
   }
 
 
   /**
-   * Log messages depending if wsClient exists or not.
+   * Log messages
+   * @param {object} echoObj - {echo_method, echo_msg, time}
    */
-  _log() {
-    if (!!this.wsClient) {
-      this.__socket_log();
-      this.__console_log();
-    } else {
-      this.__console_log();
-    }
-  }
+  _log(echoObj) {
+    this.eventEmitter.emit('echo', echoObj);
 
-
-  /**
-   * Socket log. Send wsClient message.
-   */
-  __socket_log() {
-    this.wsClient.route('/echo/distribute', this.msgObj);
-  }
-
-
-  /**
-   * Linux console log.
-   */
-  __console_log() {
     if (this.short) {
       /* SHORT PRINT */
-      const time = moment(this.msgObj.time).format('DD.MMM.YYYY HH:mm:ss.SSS');
-      if (!!this.msgObj && this.msgObj.echo_method === 'log') {
-        if (this.msgObj.echo_msg === '') { console.log(); return; }
-        console.log(chalk.greenBright(`(${time})`, this.msgObj.echo_msg)); // print string
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'objekt') {
-        console.log(chalk.blueBright(`(${time})`, JSON.stringify(this.msgObj.echo_msg))); // print stringified object
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'error') { // print error
-        console.log(chalk.redBright(`(${time})`, JSON.stringify(this.msgObj.echo_msg.message)));
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'image') { // print base64 image
-        console.log(chalk.yellowBright(`(${time})`, JSON.stringify(this.msgObj.echo_msg)));
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'input') { // print input label message
-        console.log(chalk.whiteBright(`(${time})`, JSON.stringify(this.msgObj.echo_msg)));
+      const time = moment(echoObj.time).format('DD.MMM.YYYY HH:mm:ss.SSS');
+      if (!!echoObj && echoObj.echo_method === 'log') {
+        if (echoObj.echo_msg === '') { console.log(); return; }
+        console.log(chalk.greenBright(`(${time})`, echoObj.echo_msg)); // print string
+      } else if (!!echoObj && echoObj.echo_method === 'objekt') {
+        console.log(chalk.blueBright(`(${time})`, JSON.stringify(echoObj.echo_msg))); // print stringified object
+      } else if (!!echoObj && echoObj.echo_method === 'error') { // print error
+        console.log(chalk.redBright(`(${time})`, JSON.stringify(echoObj.echo_msg.message)));
+      } else if (!!echoObj && echoObj.echo_method === 'image') { // print base64 image
+        console.log(chalk.yellowBright(`(${time})`, JSON.stringify(echoObj.echo_msg)));
       }
     } else {
       /* LONG PRINT */
-      const msg = JSON.stringify(this.msgObj, null, 4);
-      if (!!this.msgObj && this.msgObj.echo_method === 'log') {
-        if (this.msgObj.echo_msg === '') { console.log(); return; }
+      const msg = JSON.stringify(echoObj, null, 4);
+      if (!!echoObj && echoObj.echo_method === 'log') {
+        if (echoObj.echo_msg === '') { console.log(); return; }
         console.log(chalk.greenBright(msg)); // print string
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'objekt') {
+      } else if (!!echoObj && echoObj.echo_method === 'objekt') {
         console.log(chalk.blueBright(msg)); // print object
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'error') {
+      } else if (!!echoObj && echoObj.echo_method === 'error') {
         console.log(chalk.redBright(msg)); // print error
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'image') {
+      } else if (!!echoObj && echoObj.echo_method === 'image') {
         console.log(chalk.yellowBright(msg)); // print base64 image
-      } else if (!!this.msgObj && this.msgObj.echo_method === 'input') {
-        console.log(chalk.whiteBright(msg)); // print input label message
       }
     }
   }
-
 
 
 
